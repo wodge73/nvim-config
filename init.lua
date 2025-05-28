c7f59adba0dee9b39ba2ba76a2d5c11b4808818f
wodge73 <warren.legg@gmail.com>
2025-05-28 21:57:49 +0100

    updated to tokyotonigh colour scheme and changed config load

--- init.lua
+++ init.lua
@@ -2,5 +2,4 @@ require("core.options")
 require("core.mappings")
 require("core.autocommands")
 require("config.lazy")
-vim.keymap.set('n', '<leader>t', ':bo terminal<cr>a')
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.cmd.colorscheme("tokyonight")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,3 +6,6 @@ opt.shiftwidth = 2
 opt.tabstop = 2
 opt.smartindent = true
 opt.termguicolors = true
+vim.keymap.set('n', '<leader>t', ':bo terminal<cr>a')
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.termguicolors = true
